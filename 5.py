3601c5e763685243c3cad20f0b02d28d8532b74d
Javeed Ahmad <89091657+JaVD054@users.noreply.github.com>
2022-10-03 10:37:12 +0530

    Delete 5.py

--- 5.py
+++
@@ -1,7 +0,0 @@
-for _ in range(int(input())):
-    n = int(input())
-    lis = list(map(int, input().split()))
-    print(" ".join(map(str, lis[-1::-1])))
-    print(" ".join(map(lambda a: str(a + 3), lis[3::3])))
-    print(" ".join(map(lambda a: str(a - 7), lis[5::5])))
-    print(sum(lis[3:8]))
